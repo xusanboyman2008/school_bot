ded1ab33624e0fb4d981dd243ad7b126e6ab962d
unknown <abdulkhaevtollibjon55@gmail.com>
2025-05-06 01:21:23 -0700

    asdas

--- test1.py
+++ test1.py
@@ -9,19 +9,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+
 from captcha_ai import extract_numbers_from_clean_image
 from models import get_login_all
+from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument('--headless')
+options.add_argument('--headless')  # or use "--headless=new" for newer versions
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-
-driver = webdriver.Chrome(
-    service=Service("/usr/bin/chromedriver"),
-    options=options
-)
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-software-rasterizer')
+options.add_argument('--remote-debugging-port=9222')  # this avoids DevToolsActivePort error
 
 
 
